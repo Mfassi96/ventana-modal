652bf7aee2e29c4b52534d4b1e3226d7308b0398
Mauro Fassi <maurofassi17@gmail.com>
2023-02-09 20:03:44 -0300

    Estructura y estilos creados

---
+++ app.js
@@ -0,0 +1,4 @@
+//selecionar btn, modal overlay,close-btn 
+//escuchar el evento click
+//cuando el usuario clickee modal-btn agregar .OPEN-MODAL a modal-overlay 
+//cuando el usuario clickee close-btn quitar .OPEN-MODAL
