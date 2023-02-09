7f1a2c7ed00caf21262006c975bd7608c28466a9
Mauro Fassi <maurofassi17@gmail.com>
2023-02-09 20:23:56 -0300

    ventana modal finalizada

--- app.js
+++ app.js
@@ -2,3 +2,32 @@
 //escuchar el evento click
 //cuando el usuario clickee modal-btn agregar .OPEN-MODAL a modal-overlay 
 //cuando el usuario clickee close-btn quitar .OPEN-MODAL
+
+const  modalBtn=document.querySelector('.modal-btn')
+const modal=document.querySelector('.modal-overlay')
+const closeBtn=document.querySelector('.close-btn')
+
+
+modalBtn.addEventListener('click',()=>{
+    modal.classList.add('open-modal')
+})
+
+
+closeBtn.addEventListener('click',()=>{
+    modal.classList.remove('open-modal')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
